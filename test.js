36371d9469c1918fb629ce00699507fb13a8235b
Philipp Kunz <kunzphp@gmail.com>
2015-11-03 20:22:16 +0100

    updated test

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/// <reference path="typings/tsd.d.ts" />
 var smartfile = require("./index.js");
 var pr = require("pushrocks");
 pr.beautylog.info(smartfile.readFileToString("./test/mytest.txt"));
