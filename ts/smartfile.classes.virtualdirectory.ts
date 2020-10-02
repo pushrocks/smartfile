eca3e141d20807a6f4506b0a08a8a71e15656e88
Phil Kunz <gitlab@philkunz.com>
2020-10-02 13:26:53 +0000

    fix(core): update

---
+++ ts/smartfile.classes.virtualdirectory.ts
@@ -0,0 +1,31 @@
+import { Smartfile } from './smartfile.classes.smartfile';
+import * as plugins from './smartfile.plugins';
+import * as fs from './smartfile.fs';
+
+/**
+ * a virtual directory exposes a fs api
+ */
+export class VirtualDirectory {
+  private fileArray: Smartfile[] = [];
+  public static async fromFsDirPath(pathArg: string) {
+    const newVirtualDir = new VirtualDirectory();
+    newVirtualDir.addSmartfiles(await fs.fileTreeToObject(pathArg, '**/*'));
+  };
+
+  constructor() {}
+
+  public addSmartfiles(smartfileArrayArg: Smartfile[]) {
+    this.fileArray = this.fileArray.concat(smartfileArrayArg);
+  }
+
+  public async getFileByPath(pathArg: string) {
+    for (const smartfile of this.fileArray) {
+      if (smartfile.path === pathArg) {
+        return smartfile;
+      }
+    }
+  }
+
+  // TODO implement root shifting to get subdirectories as new virtual directories
+  // TODO implement root shifting to combine VirtualDirecotries in a parent virtual directory
+}
